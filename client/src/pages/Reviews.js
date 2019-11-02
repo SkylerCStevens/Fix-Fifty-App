11172494930e91320561917c31a95f3cd0f68cad
Jonathan Giler <jonathangiler@hotmail.comgit config --global user.name jonathan>
2019-11-02 01:39:06 -0400

    user pages + nav footer comp

---
+++ client/src/pages/Reviews.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Reviews = () => {
+    return (
+        <React.Fragment>
+            <h1>Reviews</h1>
+        </React.Fragment>
+    )
+}
+
+export default Reviews
