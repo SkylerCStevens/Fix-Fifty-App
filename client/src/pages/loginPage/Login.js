ead95089bee9dd2ea033c68a532a1f2d3b008778
l-yang-05 <40499685+l-yang-05@users.noreply.github.com>
2019-11-06 11:08:34 -0500

    First commit

--- client/src/pages/loginPage/Login.js
+++ client/src/pages/loginPage/Login.js
@@ -1,47 +1,46 @@
 import React, { useState } from "react";
 import axios from 'axios';
+import TextField from '@material-ui/core/TextField';
 
 const Login = () => {
-  const [firstName, setFirstName] = useState();
-  const [lastName, setLastName] = useState();
-  const [email, setEmail] = useState();
-  const [password, setPassword] = useState();
+  // const [firstName, setFirstName] = useState();
+  // const [lastName, setLastName] = useState();
+  // const [email, setEmail] = useState();
+  // const [password, setPassword] = useState();
 
 
-  const handleForm = () => {
-    axios.post("/register", {
-      firstName,
-      lastName,
-      email,
-      password
-    })
-  }
+  // const handleForm = () => {
+  //   axios.post("/register", {
+  //     firstName,
+  //     lastName,
+  //     email,
+  //     password
+  //   })
+  // }
 
-    return (
-      <React.Fragment>
-        <h1>Login</h1>
-        <form>
-          <div>
-            <label htmlFor="firstName">First Name</label>
-            <input type="firstName" name="firstName" onChange={(e) => setFirstName(e.target.value)}/>
-          </div>
-          <div>
-            <label htmlFor="lastName">Last Name</label>
-            <input type="lastName" name="lastName" onChange={(e) => setLastName(e.target.value)}/>
-          </div>
-          <div>
-            <label htmlFor="email">Email</label>
-            <input type="email" name="username" onChange={(e) => setEmail(e.target.value)}/>
-          </div>
-          <div>
-            <label htmlFor="password">Password</label>
-            <input type="password" name="password" onChange={(e) => setPassword(e.target.value)}/>
-          </div>
-          <button type="button" onClick={handleForm}>Login</button>
-        </form>
-      </React.Fragment>
-    );
-  
+  return (
+    <React.Fragment>
+      <h1>Login</h1>
+      <form>
+        <div>
+
+        </div>
+        <label htmlFor="firstName">First Name</label>
+        <TextField
+          variant="outlined"
+          margin="normal"
+          required
+          name="firstName"
+          label="First Name"
+          type="required"
+          id="firstName"
+          onChange={(e) => setFirstName(e.target.value)}
+        />
+        <button type="button" onClick={handleForm}>Login</button>
+      </form>
+    </React.Fragment>
+  );
+
 }
 
 export default Login;
