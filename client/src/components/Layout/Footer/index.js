53e70d244f91df405bcc6ba08a7a8ee69b8f1d84
Jonathan Morales <johnathanmorales@Administrators-MacBook-Pro-67.local>
2019-11-07 10:38:36 -0500

    Refactor front-end, folder, and file structure

---
+++ client/src/components/Layout/Footer/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <div>
+      <p>footerrrrr</p>
+      <h3>whats up</h3>
+    </div>
+  );
+};
+
+export default Footer;
---
+++ client/src/components/Layout/Header/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => {
+  return (
+    <div>
+      <p>headerrr</p>
+    </div>
+  );
+};
+
+export default Header;
