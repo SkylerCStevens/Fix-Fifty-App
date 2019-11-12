5efa80adfd1004dddeab1dc2d019794b16308854
l-yang-05 <40499685+l-yang-05@users.noreply.github.com>
2019-11-12 10:54:11 -0500

    Edited Nav on mobile

--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -1,15 +1,9 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
+import './index.css'
 
 const Nav = () => (
   <nav className="nav">
-    <NavLink exact to="/" className="link">
-      <img
-        className='logo'
-        // src={require('../img/logo.png')}
-        alt='clymb logo'
-      ></img>
-    </NavLink>
   <NavLink to="/" className="link">Job Listing</NavLink>
   <NavLink to="/schedule" className="link">Schedule Meeting</NavLink>
   <NavLink to="/profile" className="link">Profile</NavLink>
