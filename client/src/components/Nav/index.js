ef2bd85439d262d710db78a7a7618a617d6cdd73
Jonathan Giler <jonathangiler@hotmail.comgit config --global user.name jonathan>
2019-11-13 00:35:29 -0500

    profile pages + job listings

--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -6,7 +6,8 @@ const Nav = () => (
   <nav className="nav">
   <NavLink to="/" className="link">Job Listing</NavLink>
   <NavLink to="/schedule" className="link">Schedule Meeting</NavLink>
-  <NavLink to="/profile" className="link">Profile</NavLink>
+  <NavLink to="/profileUnverified" className="link">profileUnverified</NavLink>
+  <NavLink to="/profileVerified" className="link">profileVerified</NavLink>
   </nav>
 );
 
--- client/src/index.js
+++ client/src/index.js
@@ -10,10 +10,13 @@ import Header from "./components/Layout/Header";
 import Login from "./components/Login";
 import Signup from "./components/Register";
 import Footer from "./components/Layout/Footer";
-import Joblistings from "./components/Joblistings";
+import Joblistings from "./components/Joblistings/Joblistings";
 import Nav from "./components/Nav";
 import Schedule from './components/Schedule/index';
-import Profile from './components/Profile/index';
+import Unverified from './components/Profile/Unverified';
+import Verified from './components/Profile/Verified';
+import '../src/assets/styles/unverified.scss'
+import '../src/assets/styles/jobs.scss'
 
 // creating a store for redux with the redux chrome extension
 export const store = createStore(
@@ -31,7 +34,8 @@ const Routing = (
         <Route path="/login" component={Login} />
         <Route path="/signup" component={Signup} />
         <Route path="/schedule" component={Schedule} />
-        <Route path="/profile" component={Profile} />
+        <Route path="/profileUnverified" component={Unverified} />
+        <Route path="/profileVerified" component={Verified} />
       </main>
     </Switch>
     <Footer />
