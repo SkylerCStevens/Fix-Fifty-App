b19130a7452d96d0b1a6b2387af3190317602462
Skyler Stevens <sabrinastevens@AdminisatorsMBP.lan>
2019-11-12 20:14:00 -0500

    Add joblisting cards

--- client/src/components/Joblistings/index.js
+++ client/src/components/Joblistings/index.js
@@ -1,63 +1,135 @@
 import React from "react";
 import "./jobs.css"
+import { makeStyles } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
 
-
+const useStyles = makeStyles(theme => ({
+    card: {
+      minWidth: 275,
+      marginBottom: 50,
+    },
+    title: {
+      fontSize: 14,
+    },
+    pos: {
+      marginBottom: 12,
+    },
+    button: {
+        margin: theme.spacing(1),
+      },
+      input: {
+        display: 'none',
+      },
+  }));
 
 function Joblistings() {
+    const classes = useStyles();
     return (
-        <div>
-            <h1>JOBS</h1>
-            <div className="product-box">
-                <img
-                  src="https://image.flaticon.com/icons/png/512/46/46405.png"
-                  alt="Construction Company"
-                  className="img card-img-top logo"
-                ></img>
-                <div className="card-body">
-                  <h3 className="card-header">Paint a House</h3>
-                  <p className="card-subtitle">Construction Company</p>
-                  <p className="card-text ml-3">Tasked to paint the outside of a 3 story house in 5 days</p>
-                  <p>$2000</p>
-                  <p>5 day deadline</p>
-                  <p>10 days left to apply</p>
-                </div>
-                <button>APPLY</button>
-              </div>
+        <div className="container">
+            <h1 className="text-center mt-5 mb-5">JOBS</h1>
+            <Card className={classes.card}>
+      <CardContent>
+        <Typography className={classes.title} color="textSecondary" gutterBottom>
+          Construction Company
+        </Typography>
+        <Typography variant="h5" component="h2">
+          Paint a House
+        </Typography>
+        <Typography className={classes.pos} color="textSecondary">
+          $2000
+        </Typography>
+        <Typography variant="body2" component="p">
+          Tasked to paint the outside of a 3 story house.
+        </Typography>
+        <Typography className={classes.pos} color="textSecondary">
+          3 day deadline
+        </Typography>
+      </CardContent>
+      <CardActions>
+      <Button variant="outlined" color="primary" className={classes.button}>
+        APPLY
+      </Button>
+      </CardActions>
+    </Card>
 
-              <div className="product-box">
-                <img
-                  src="https://image.flaticon.com/icons/png/512/46/46405.png"
-                  alt="Construction Company"
-                  className="img card-img-top logo"
-                ></img>
-                <div className="card-body">
-                  <h3 className="card-header">Roof Repair</h3>
-                  <p className="card-subtitle">Construction Company</p>
-                  <p className="card-text ml-3">Tasked to repair one of our clients roofs within 3 days</p>
-                  <p>$2000</p>
-                  <p>3 day deadline</p>
-                  <p>1 day left to apply</p>
-                </div>
-                <button>APPLY</button>
-              </div>
+    <Card className={classes.card}>
+      <CardContent>
+        <Typography className={classes.title} color="textSecondary" gutterBottom>
+          Construction Company
+        </Typography>
+        <Typography variant="h5" component="h2">
+          Build a Shed
+        </Typography>
+        <Typography className={classes.pos} color="textSecondary">
+          $2500
+        </Typography>
+        <Typography variant="body2" component="p">
+          Tasked to build a small utility shed behind one of our client's houses.
+        </Typography>
+        <Typography className={classes.pos} color="textSecondary">
+          5 day deadline
+        </Typography>
+      </CardContent>
+      <CardActions>
+      <Button variant="outlined" color="primary" className={classes.button}>
+        APPLY
+      </Button>
+      </CardActions>
+    </Card>
 
+    <Card className={classes.card}>
+      <CardContent>
+        <Typography className={classes.title} color="textSecondary" gutterBottom>
+          Realty Company
+        </Typography>
+        <Typography variant="h5" component="h2">
+          Clean a House
+        </Typography>
+        <Typography className={classes.pos} color="textSecondary">
+          $1500
+        </Typography>
+        <Typography variant="body2" component="p">
+        Clean a 3 bedroom 1500sq ft. home before new tenants move in.
+        </Typography>
+        <Typography className={classes.pos} color="textSecondary">
+          3 day deadline
+        </Typography>
+      </CardContent>
+      <CardActions>
+      <Button variant="outlined" color="primary" className={classes.button}>
+        APPLY
+      </Button>
+      </CardActions>
+    </Card>
 
-              <div className="product-box">
-                <img
-                  src="https://image.flaticon.com/icons/png/512/48/48821.png"
-                  alt="Realty Company"
-                  className="img card-img-top logo"
-                ></img>
-                <div className="card-body">
-                  <h3 className="card-header">Clean a House</h3>
-                  <p className="card-subtitle">Realty Company</p>
-                  <p className="card-text ml-3">Tasked to clean a 3 bedroom 1500 sq ft house in 2 days</p>
-                  <p>$2000</p>
-                  <p>2 day deadline</p>
-                  <p>10 days left to apply</p>
-                </div>
-                <button>APPLY</button>
-              </div>
+    <Card className={classes.card}>
+      <CardContent>
+        <Typography className={classes.title} color="textSecondary" gutterBottom>
+          Realty Company
+        </Typography>
+        <Typography variant="h5" component="h2">
+          Replace Bedroom Carpet
+        </Typography>
+        <Typography className={classes.pos} color="textSecondary">
+          $800
+        </Typography>
+        <Typography variant="body2" component="p">
+        Replace carpet of 400sq ft. bedroom.
+        </Typography>
+        <Typography className={classes.pos} color="textSecondary">
+          1 day deadline
+        </Typography>
+      </CardContent>
+      <CardActions>
+      <Button variant="outlined" color="primary" className={classes.button}>
+        APPLY
+      </Button>
+      </CardActions>
+    </Card>
         </div>
     )
 
